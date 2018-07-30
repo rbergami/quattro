80dfe19b0e90ed717d8adbc9ed39644c169ced98
rbergami <rbergami@redhat.com>
2018-07-30 17:15:18 +0200

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ $db_user="mrmoon";
 $db_password="mrmoon";
 
 //connessione al db server
-$conn= mysqli_connect($db_server, $db_user, $db_password)
+$conn= mysql_connect($db_server, $db_user, $db_password)
 	or die("Errore! Impossibile connettersi al db server");
 
 //nome database
@@ -14,4 +14,7 @@ $db_nomedb="recensioni";
 
 //sceglie il db
 if (!mysql_select_db($db_nomedb, $conn)) die ("Errore! Impossibile connettersi al database $db_nomedb");
+
+error_reporting(E_ALL ^ E_DEPRECATED);
+
 ?>
